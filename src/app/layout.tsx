bb8a52b6ec16183713adf411822ddce4ed8e39f5
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-07 15:58:09 +0330

    import Layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Layout from "@/components/layout/Layout";
 
 
 export const metadata: Metadata = {
@@ -15,7 +16,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {children}
+        <Layout>
+          {children}
+        </Layout>
       </body>
     </html>
   );
