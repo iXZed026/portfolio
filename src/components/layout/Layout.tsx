ec7dc9bd001af473fb013369085a2f1656b75a3d
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-10 13:35:11 +0330

    import in Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,17 @@
 import { ChildrenProps } from '@/types/children'
 import React from 'react'
+import NavBar from '../navbar/NavBar'
+import ActiveHumbProvider from '@/context/ActiveHumbProvider'
+import NavBarWrapper from '../navbar/navbar-wrapper/NavBarWrapper'
 
 function Layout({ children }: ChildrenProps) {
     return (
         <>
+            <ActiveHumbProvider>
+                <NavBar>
+                    <NavBarWrapper />
+                </NavBar>
+            </ActiveHumbProvider>
             {children}
         </>
     )
