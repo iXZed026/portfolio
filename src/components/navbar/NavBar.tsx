1e35c900777f603ef30881a5691aca46669a8f7e
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-10 13:30:38 +0330

    create NavBar component

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,38 @@
+"use client"
+import { ChildrenProps } from '@/types/children'
+import React, { useEffect, useRef } from 'react'
+
+function NavBar({ children }: ChildrenProps) {
+
+    const headerRef = useRef<HTMLDivElement | null>(null)
+
+    const headerStyle = "w-full fixed py-4"
+
+    useEffect(() => {
+
+        function calculateScrollY() {
+            if (headerRef.current) {
+                if (window.scrollY > 5) {
+                    headerRef.current.className = `${headerStyle} backdrop-blur-md bg-gray-900/50 shadow-lg`
+                } else {
+                    headerRef.current.className = headerStyle
+                }
+            }
+        }
+
+        window.addEventListener("scroll", calculateScrollY)
+
+        return () => {
+            window.removeEventListener("scroll", calculateScrollY)
+        }
+
+    }, [])
+
+    return (
+        <header className={headerStyle} ref={headerRef}>
+            {children}
+        </header>
+    )
+}
+
+export default NavBar
