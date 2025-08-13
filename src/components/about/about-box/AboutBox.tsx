5d4352c459c2cb2fd7e6de6b6e044a92969cd00a
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-13 17:34:39 +0330

    refactor tailwind style

--- src/components/about/about-box/AboutBox.tsx
+++ src/components/about/about-box/AboutBox.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 
 function AboutBox({ children, title }: ChildrenProps & { title: string }) {
     return (
-        <div
-            className='bg-[var(--bg-gray-color)] p-10 rounded-xl col-span-6 border-[1px] border-gray-500/50'
-        >
+        <div>
             <h2 className='text-2xl font-semibold mb-5'>{title}</h2>
             <div>
                 {children}
