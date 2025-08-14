aade28fdeb03b0abb4aa69fa2ca9c3d89ceb36c6
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-14 11:05:03 +0330

    create Projects.tsx page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Projects() {
+  return (
+    <div className='bg-page center-screen py-20 overflow-x-hidden'>
+        Projects
+    </div>
+  )
+}
+
+export default Projects
