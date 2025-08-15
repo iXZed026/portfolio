65c08a4dd97faefda4c2ebc8d1bd775012670f8e
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-15 11:55:57 +0330

    create ProjectBox.tsx component

---
+++ src/components/projects/project-box/ProjectBox.tsx
@@ -0,0 +1,63 @@
+import Image from 'next/image'
+import React from 'react'
+import { FaGithub } from 'react-icons/fa'
+import { CgWebsite } from "react-icons/cg";
+import { IProjects } from '@/app/api/projects/route';
+
+async function ProjectBox() {
+
+    const res = await fetch("http://localhost:3000/api/projects",{
+        cache:'force-cache',
+    });
+    let projects;
+
+    res.ok ? projects = await res.json() : projects = [];
+
+    return (
+        <>
+            {
+                projects.map((project: IProjects) => (
+                    <div key={project.id} className='top-projects-animation span-3 border-[0.5px] border-gray-600/50 bg-[var(--bg-gray-color)] rounded-lg project-box'>
+                        <div className='mb-4'>
+                            <Image
+                                className='w-full rounded-lg'
+                                src={project.image}
+                                width={390}
+                                height={390}
+                                priority={true}
+                                quality={60}
+                                alt='demo image'
+                            />
+                        </div>
+                        <div className='p-3 mb-2'>
+                            <h5 className='text-xl font-semibold mb-3'>{project.projectName}</h5>
+                            <h6 className='text-[var(--desc-color)] mb-3 text-md'>{project.describtion}</h6>
+                            <div className='start-screen gap-5'>
+                                {project.technologies.map((tech,key) => <span key={key} className='project-skill'>{tech}</span>)}
+
+                            </div>
+                            <div className='center-screen gap-10 mt-7'>
+                                <a
+                                    className='bg-gray-600/70 px-5 py-2 rounded-md between-screen gap-1 hover'
+                                    href={project.hrefs[0]}
+                                >
+                                    <FaGithub className='text-xl' />
+                                    منبع
+                                </a>
+                                <a
+                                    className='bg-transparent border-1 border-gray-500/60 px-5 py-2 between-screen gap-1 rounded-md hover'
+                                    href={project.hrefs[1]}
+                                >
+                                    <CgWebsite className='text-xl' />
+                                    دیدن
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default ProjectBox
