29cccadd6cda53c7956dbe1c66d684b039c28bbc
Danyal Lotfi <danyal.titanka24@gmail.com>
2025-08-16 17:54:28 +0330

    fix errors and warnings

--- src/components/about/skill-box/skill-box-wraper/SkillBoxWraper.tsx
+++ src/components/about/skill-box/skill-box-wraper/SkillBoxWraper.tsx
@@ -7,8 +7,11 @@ import { FaHtml5, FaCss3, FaJs, FaReact, FaBootstrap, FaGit, FaGithub } from "re
 import { RiNextjsFill, RiTailwindCssFill } from "react-icons/ri";
 import { SiRedux, SiTypescript } from 'react-icons/si';
 
+interface SkillBoxWraperProps {
+    skills: ISkills[]
+}
 
-function SkillBoxWraper({ skills }: { skills: ISkills[] }) {
+function SkillBoxWraper({ skills }: SkillBoxWraperProps) {
 
     const { showMore } = useShowMoreContext();
     const [visableSkills, setVisableSkills] = useState<ISkills[]>([])
@@ -29,42 +32,37 @@ function SkillBoxWraper({ skills }: { skills: ISkills[] }) {
             SiTypescript,
         ]
 
-        let Icon = icons[Counter];
+        const Icon = icons[Counter];
         return <Icon className="text-[#50A2FF] text-xl" />
     }
 
     useEffect(() => {
-
         if (showMore) {
             setVisableSkills(skills)
         } else {
             setVisableSkills(skills.slice(0, 5))
         }
-
-    }, [showMore]);
-
+    }, [showMore, skills]);
 
     return (
-
-        visableSkills.map((item: ISkills, i: number) => (
-            <div className='my-5' key={item.id}>
-                <div className='start-screen mb-3 gap-4'>
-                    <div className='p-2 rounded-md bg-[#1D3063]'>
-                        {
-                            showSkill(i)
-                        }
+        <>
+            {visableSkills.map((item: ISkills, i: number) => (
+                <div className='my-5' key={item.id}>
+                    <div className='start-screen mb-3 gap-4'>
+                        <div className='p-2 rounded-md bg-[#1D3063]'>
+                            {showSkill(i)}
+                        </div>
+                        <span className='opacity-70 text-sm'>{item.name}</span>
+                        <span className='opacity-70 text-sm'>{item.mastery}%</span>
                     </div>
-                    <span className='opacity-70 text-sm'>{item.name}</span>
-                    <span className='opacity-70 text-sm'>{item.mastery}%</span>
-                </div>
-                <div>
-                    <div className='bg-gray-600 w-full h-[10px] rounded-xl'>
-                        <div style={{ width: `${item.mastery}%` }} className={` h-[10px] skill-color rounded-xl`}></div>
+                    <div>
+                        <div className='bg-gray-600 w-full h-[10px] rounded-xl'>
+                            <div style={{ width: `${item.mastery}%` }} className="h-[10px] skill-color rounded-xl"></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ))
-
+            ))}
+        </>
     )
 }
 
